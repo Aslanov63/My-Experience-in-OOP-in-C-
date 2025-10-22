e566499dcf9eba017308b23f31c50f92fb81d3aa
Ali Aslanov <ali.aslanov.2007@gmail.com>
2025-10-22 16:55:16 +0000

    Сохранён основной код программы с меню и пустыми функциями

--- Lab1.cpp
+++ Lab1.cpp
@@ -62,34 +62,67 @@ void countingSort1(vector<string>& quotes) {
 
 void countingSort2(vector<string>& quotes) {
     size_t max_len = 0;
-    for (const auto& q : quotes) {
+    for (const auto& q : quotes) { ///find the longest word
         if (q.length() > max_len) {
             max_len = q.length();
         }
     }
 
     vector<vector<string>> buckets(max_len+1);
+
     for (const auto& q : quotes) {
         buckets[q.size()].push_back(q);
     }
+    quotes.clear();
 
     for (size_t i = 0; i < buckets.size(); ++i) {
         if (!buckets[i].empty()) {
-            cout << "[" << i << "] (" << buckets[i].size() << "):" << endl;
             for (const auto& q : buckets[i]) {
-                cout << q << endl;
+                quotes.push_back(q);
+             
             }
-            cout << endl;
+           
         }
     }
+    printQuotes(quotes);
+
+}
+
+void quickSortHelper(vector<string>& quotes, int left, int right) {
+    if (left >= right) return; 
+    int i = left, j = right;
+    string pivot = quotes[left + (right - left) / 2]; 
+
+    while (i <= j) { 
+        while (quotes[i] < pivot) i++;
+        while (quotes[j] > pivot) j--; 
+        if (i <= j) {
+            swap(quotes[i], quotes[j]); 
+            i++; 
+            j--; 
+        }
+    }
+
+    quickSortHelper(quotes, left, j); 
+    quickSortHelper(quotes, i, right); 
 }
 
 void quickSort(vector<string>& quotes) {
-    // пока пусто
+    quickSortHelper(quotes, 0, quotes.size() - 1); 
+    printQuotes(quotes); 
 }
 
 void insertionSort(vector<string>& quotes) {
-    // пока пусто
+    for (size_t i = 1; i < quotes.size(); ++i) {
+        string key = quotes[i];
+        int j = i - 1;
+        while (j >= 0 && quotes[j] > key) {
+            quotes[j + 1] = quotes[j];
+            j--;
+        }
+        quotes[j + 1] = key;
+    }
+    printQuotes(quotes);
 }
 
 int main() {
@@ -109,10 +142,10 @@ int main() {
     cout << "MENU" << endl;
     cout << "1. Display all quotes" << endl;
     cout << "2. Brick Sort" << endl;
-    cout << "3. Counting sort" << endl;
-    cout << "4. Quick-sort(TURBO)" << endl;
-    cout << "5. Insertion sort(SLOW) " << endl;
-    cout << "6. Counting sort by LEngth" << endl;
+    cout << "3. Counting sort by first letter" << endl;
+    cout << "4. Quick-sort (TURBO)" << endl;
+    cout << "5. Insertion sort (SLOW)" << endl;
+    cout << "6. Counting sort by Length" << endl;
     cout << "7. Exit" << endl;
     int choice;
     cout << "Enter your choice: ";
